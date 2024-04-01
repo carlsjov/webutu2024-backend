824ac90d34d95fbd1b7b75f25161029eac3c8035
caersj@utu.fi <caersj@utu.fi>
2024-04-01 20:33:36 +0300

    tehty part3-11

---
+++ mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password to continue')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://carlsjov:${password}@cluster0.moedpph.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const name = process.argv[3]
+const number = process.argv[4]
+
+const person = new Person({
+  name: name,
+  number: number,
+})
+
+if (process.argv.length == 3) {
+    console.log('Phonebook:')
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(person.name, person.number)
+        })
+    mongoose.connection.close()
+    })
+} else {
+    person.save().then(result => {
+        console.log(`added ${name} number ${number}!`)
+        mongoose.connection.close()
+    })
+}
