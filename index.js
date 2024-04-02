fce44efe8ceb41c6b6089779f03f6c1ee013ca8e
Carl Sjövall <caersj@utu.fi>
2024-04-02 19:08:31 +0300

    lisätty modules

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 var morgan = require('morgan')
 const app = express()
 const cors = require('cors')
+const NOTE = require('./models/person')
 
 app.use(cors())
 app.use(express.json())
@@ -83,6 +85,6 @@ app.post('/api/persons', (req, res) => {
   }
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
--- models/person.js
+++ models/person.js
@@ -1,1 +1,32 @@
-const mongoose = re
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
+const password = process.argv[2]
+
+const url = process.env.MONGODB_URI
+//  `mongodb+srv://carlsjov:${password}@cluster0.moedpph.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+console.log('connectiong to', url)
+
+
+mongoose.connect(url).then(result => {
+    console.log('connected to MongoDB')
+}).catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+})
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Person', personSchema)
