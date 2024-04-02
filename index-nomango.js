b3f7092cbeb480a14ddd8e7bb240832218926f59
Carl Sjövall <caersj@utu.fi>
2024-04-02 18:51:43 +0300

    tehty indexin kopio

---
+++ index-nomango.js
@@ -0,0 +1,88 @@
+const express = require('express')
+var morgan = require('morgan')
+const app = express()
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+app.use(morgan('tiny'))
+app.use(express.static('dist'))
+
+let persons = [
+    {
+      name: "Arto Hellas",
+      number: "040-123456",
+      id: 1
+    },
+    {
+      name: "Ada Lovelace",
+      number: "39-44-5323523",
+      id: 2
+    },
+    {
+      name: "Dan Abramov",
+      number: "12-43-234345",
+      id: 3
+    },
+    {
+      name: "Mary Poppendieck",
+      number: "39-23-6423122",
+      id: 4
+    }
+  ]
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+    const date = new Date()
+    const text = `<p>Phonebook has info for ${persons.length} people <br> ${date}</p>` 
+    console.log(date)
+    res.send(text)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const randomId = Math.floor(Math.random()*1000)
+  const person = req.body
+  if (person.number && person.name) {
+    person.id = randomId
+    console.log(person)
+    const checkName = persons.find(check => check.name === person.name)
+    const checkNumber = persons.find(check => check.number === person.number)
+    console.log(checkName, checkNumber)
+    if (checkName || checkNumber) {
+      return res.status(400).json({
+        error: 'name or number already in use'
+      })
+    } else {
+      persons = persons.concat(person)
+      res.json(person)
+    }
+  } else {
+    return res.status(400).json({
+      error: 'name or number missing'
+    })
+  }
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
